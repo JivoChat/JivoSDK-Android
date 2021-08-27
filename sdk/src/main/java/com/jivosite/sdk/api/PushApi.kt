9febeec9f3e57113839ed89493326be08a78ff80
atavtorkin <tavtorkin@jivosite.com>
2021-08-27 15:26:01 +0300

    MOB-3429: send delivery status

--- sdk/src/main/java/com/jivosite/sdk/api/PushApi.kt
+++ sdk/src/main/java/com/jivosite/sdk/api/PushApi.kt
@@ -5,6 +5,7 @@ import com.jivosite.sdk.model.pojo.push.Device
 import com.jivosite.sdk.model.pojo.response.Response
 import com.jivosite.sdk.network.response.ApiResponse
 import com.jivosite.sdk.network.retrofit.ChangeUrlInterceptor.Companion.NODE
+import com.jivosite.sdk.network.retrofit.ChangeUrlInterceptor.Companion.PUSH
 import com.jivosite.sdk.network.retrofit.ChangeUrlInterceptor.Companion.URL
 import retrofit2.http.*
 
@@ -22,6 +23,14 @@ interface PushApi {
         @Path("siteId") siteId: Long,
         @Path("widgetPublicId") widgetPublicId: String,
         @Body body: Device
+    ): LiveData<ApiResponse<Response>>
+
+    @POST("push/delivery/{site_id}/{client_id}/{push_id}?platform=android")
+    @Headers("$URL:$PUSH")
+    fun postPushDelivery(
+        @Path("site_id") siteId: String,
+        @Path("client_id") clientId: String,
+        @Path("push_id") pushId: String
     ): LiveData<ApiResponse<Response>>
 
 }
