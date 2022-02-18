2f02b731ea40f0007a5d8d6a0cfd14ba0a264a7e
Denis Zolotarev <judaeus@gmail.com>
2022-02-18 13:52:37 +0200

    MOB-3833: add sample module

---
+++ sample/src/main/java/com/jivosite/jivosdk_android_sample/App.kt
@@ -0,0 +1,34 @@
+package com.jivosite.jivosdk_android_sample
+
+import android.app.Application
+import com.jivosite.sdk.Jivo
+import com.jivosite.sdk.support.builders.Config
+
+/**
+ * Created on 4/19/21.
+ *
+ * @author Alexander Tavtorkin (tavtorkin@jivosite.com)
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Jivo.init(
+            appContext = this,
+            widgetId = "Q7BcPYNqCG"
+        )
+        Jivo.setConfig(
+            Config.Builder()
+                .setLogo(R.drawable.vic_jivosdk_logo)
+                .setBackground(R.drawable.bg_jivosdk_appbar)
+                .setTitle(R.string.jivosdk_title)
+                .setTitleTextColor(R.color.white)
+                .setSubtitle(R.string.jivosdk_subtitle)
+                .setSubtitleTextColor(R.color.white)
+                .setSubtitleTextColorAlpha(0.6f)
+                .setWelcomeMessage(R.string.jivosdk_welcome)
+                .setOutgoingMessageColor(Config.Color.GREY)
+                .build()
+        )
+    }
+}
